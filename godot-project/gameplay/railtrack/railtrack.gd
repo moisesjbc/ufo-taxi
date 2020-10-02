27753fc4543e1e452e7f5ab520ed4af3bb4335f5
Moisés J <moisesjbc@gmail.com>
2020-10-03 00:24:10 +0100

    Added dummy railtrack scene

---
+++ godot-project/gameplay/railtrack/railtrack.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+var nodes = [
+	Vector2(150, 150),
+	Vector2(300, 300),
+	Vector2(200, 400),
+	Vector2(125, 500)
+]
+
+func _draw():
+	for i in range(len(nodes) - 1):
+		draw_line(nodes[i], nodes[i+1], Color.red, 5)
+	draw_line(nodes[len(nodes) - 1], nodes[0], Color.red, 5)
