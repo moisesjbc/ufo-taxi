f7ac2bfb2ee1eb6377018c877caf1f531a8afdec
Moisés J <moisesjbc@gmail.com>
2020-10-04 20:08:50 +0100

    Exit button disabled when in HTML5

--- godot-project/menus/main_menu/main_menu.gd
+++ godot-project/menus/main_menu/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+func _ready():
+	if OS.get_name() == "HTML5":
+		$margin_container/vbox_container/buttons_container/exit_button.visible = false
+
+
 func _on_start_game_button_pressed():
 	get_tree().change_scene("res://gameplay/main/main.tscn")
 
