bb034fb5a7dff19f59937c721ba56d1ea179bbbc
Moisés J <moisesjbc@gmail.com>
2020-10-04 20:35:45 +0100

    Improved warning label visibility

--- godot-project/gameplay/warning_label/warning_label.gd
+++ godot-project/gameplay/warning_label/warning_label.gd
@@ -1,15 +1,20 @@
-extends Label
+extends Node2D
 
 func set_warning(text):
-	self.text = text
+	$color_rect/label.text = text
+	$color_rect.rect_size.x = 10 * len(text)
 	visible = true
 	$display_timer.start(2)
 	set_physics_process(true)
 	
 func _physics_process(delta):
-	set_global_position(get_global_mouse_position())
+	var mouse_global_position = get_global_mouse_position()
+	if mouse_global_position.x < 800:
+		set_global_position(get_global_mouse_position())
+	else:
+		set_global_position(get_global_mouse_position() - Vector2($color_rect.rect_size.x, 0))
 
 func _on_display_timer_timeout():
-	text = ""
+	$color_rect/label.text = ""
 	visible = false
 	set_physics_process(false)
