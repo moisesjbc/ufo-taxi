b8d0121853f3aafebba5b0291badfbc62ce0b0c4
Moisés J <moisesjbc@gmail.com>
2020-10-23 22:38:01 +0100

    Added support for screen touch selection

--- godot-project/gameplay/railtrack/states/selection.gd
+++ godot-project/gameplay/railtrack/states/selection.gd
@@ -5,7 +5,7 @@ export var node_selection_distance: int = 50
 export var edge_selection_distance: int = 100
 
 func input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion or event is InputEventScreenTouch:
 		var mouse_position = railtrack.to_local(event.position)
 		var node_index = railtrack.get_closest_node(mouse_position)
 		if node_index != null:
@@ -18,5 +18,8 @@ func input(event):
 				var closest_edge = railtrack.get_closest_edge(mouse_position, node_index, edge_selection_distance)
 				railtrack.highlight_edge(closest_edge)
 				
+		if event is InputEventScreenTouch and event.pressed:
+			railtrack.confirm_current_selection()
+		
 	elif event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		railtrack.confirm_current_selection()
