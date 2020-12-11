e770a9b7e4e89c1027758598acc0f2581ddc1ad9
Moisés J <moisesjbc@gmail.com>
2020-12-11 19:22:12 +0000

    Removed level edit buttons for standalone builds

--- godot-project/menus/level_selector/level_options/level_options.gd
+++ godot-project/menus/level_selector/level_options/level_options.gd
@@ -4,6 +4,12 @@ signal play_button_pressed
 signal edit_button_pressed
 
 
+func _ready():
+	# Only when playing from editor add button for editing the level level.
+	# Source: <https://godotengine.org/qa/58519/check-if-game-is-exported>
+	$edit_button.visible = not OS.has_feature("standalone")
+
+
 func set_level_info(level_index: int, level_id: int, enabled: bool):
 	$play_button.text = str(level_index)
 	if enabled:
