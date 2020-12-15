e0f9fdac6c4a7815e35126d9182d0a97b48f7788
Moisés J <moisesjbc@gmail.com>
2020-12-15 18:24:59 +0000

    Fixed "Fullscreen: OFF" label when entering options menu in fullscreen mode

--- godot-project/menus/options_menu/options_menu.gd
+++ godot-project/menus/options_menu/options_menu.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+func _ready():
+	update_fullscreen_button_label()
+
 func set_volume(bus_name, value, min_volume, max_volume):
 	value = min_volume + ((max_volume - min_volume) * (value / 100))
 	if value != min_volume:
@@ -27,7 +30,11 @@ func _on_return_button_pressed():
 
 func _on_change_fullscreen_button_toggled(button_pressed):
 	OS.window_fullscreen = button_pressed
-	if button_pressed:
+	update_fullscreen_button_label()
+
+
+func update_fullscreen_button_label():
+	if OS.window_fullscreen:
 		$margin_container/vbox_container/options_container/fullscreen_container/change_fullscreen_button.text = 'Fullscreen: ON'
 	else:
 		$margin_container/vbox_container/options_container/fullscreen_container/change_fullscreen_button.text = 'Fullscreen: OFF'
