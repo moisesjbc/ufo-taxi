d44a02ef9d675c680a612e9d605a97b826e0cee7
Moisés J <moisesjbc@gmail.com>
2020-12-17 16:59:31 +0000

    Fixed reseting level when player had all cows trigerring level win

--- godot-project/gameplay/pickup_area/pickup_area.gd
+++ godot-project/gameplay/pickup_area/pickup_area.gd
@@ -6,9 +6,12 @@ func _ready():
 	update_label()
 
 func _on_pickup_area_body_entered(body):
+	# Without the cooldown timer, reseting current level while player is on a
+	# pickup area would trigger this collision while pickup area is generated
+	# but still hasn't be placed on its final position.
 	# When playing level from level editor, pickup areas from editor are hiden
 	# and replaced by realtime copies. Disable collisions for the not visible.
-	if visible and body.name == 'player' and has_passengers:
+	if $cooldown_timer.time_left <= 0 and visible and body.name == 'player' and has_passengers:
 		has_passengers = false
 		update_label()
 		body.pick_passenger()
